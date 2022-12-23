3d77515b0d4ba695f8cd3112b5bf472dafe950a8
Ashish Ranjan <akp9801@gmail.com>
2022-12-23 21:19:41 +0530

    multiple props

--- src/index.js
+++ src/index.js
@@ -6,8 +6,16 @@ import { MyDocker } from './components/card';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <MyDocker status="running" terminal="Bash Shell"/>
-    <MyDocker status="stopped" terminal="redhat terminal" />
+    <MyDocker 
+        status="running"
+        terminal="Bash Shell"
+        logoimg="https://pbs.twimg.com/profile_images/1123568405682970629/SpLGmNZr_400x400.png"
+    />
+    <MyDocker 
+        status="stopped"
+        terminal="redhat terminal" 
+        logoimg="https://assets.ubuntu.com/v1/29985a98-ubuntu-logo32.png"
+    />
     <MyDocker status="running" />
     <MyDocker status="running" />
     <MyDocker status="paused" />
